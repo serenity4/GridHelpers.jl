02982eb767cde54e9945e79fb783f0ccaa94965d
serenity4 <cedric.bel@hotmail.fr>
2023-04-28 15:05:01 +0200

    Add Cell and GridPoint utilities

--- src/GridHelpers.jl
+++ src/GridHelpers.jl
@@ -1,5 +1,62 @@
 module GridHelpers
 
-# Write your package code here.
+struct GridPoint
+  coords::Tuple{Int,Int}
+end
+
+Base.getindex(point::GridPoint) = point.coords
+Base.iterate(point::GridPoint, args...) = iterate(point[], args...)
+Base.length(point::GridPoint) = length(point[])
+Base.convert(::Type{GridPoint}, coords::Tuple{Int,Int}) = GridPoint(coords)
+
+function Base.getproperty(point::GridPoint, name::Symbol)
+  name === :bottom && return point .+ (0, -1)
+  name === :top && return point .+ (0, 1)
+  name === :left && return point .+ (0, -1)
+  name === :right && return point .+ (0, 1)
+  getfield(point, name)
+end
+
+"""
+Cell defined by four corner points.
+"""
+struct Cell
+  bottom_left::GridPoint
+  bottom_right::GridPoint
+  top_right::GridPoint
+  top_left::GridPoint
+end
+
+Base.length(::Cell) = 4
+Base.iterate(cell::Cell, args...) = iterate((cell.bottom_left, cell.bottom_right, cell.top_left, cell.top_right), args...)
+Base.getindex(cell::Cell, i) = iterate(cell, i)[1]
+
+Base.getindex(A::AbstractArray, point::GridPoint) = A[point[]...]
+Base.setindex!(A::AbstractArray, value, point::GridPoint) = A[point[]...] = value
+
+nearest((x, y)) = Int.(round.((x, y)))
+
+Cell(x, y) = Cell((x, y))
+function Cell((x, y))
+  (i, j) = Int.(floor.((x, y)))
+  Cell((i, j), (i + 1, j), (i + 1, j + 1), (i, j + 1))
+end
+
+function bilinear_weights(cell::Cell, (x, y))
+  (cx, cy) = cell.bottom_left
+  w1 = (-(x - (1 + cx)) * -(y - (1 + cy)))
+  w2 = ((x - cx) * -(y - (1 + cy)))
+  w3 = (-(x - (1 + cx)) * (y - cy))
+  w4 = ((x - cx) * (y - cy))
+  (w1, w2, w3, w4)
+end
+
+function interpolate_bilinear(A, position)
+  cell = Cell(position)
+  weights = bilinear_weights(cell, position)
+  sum(i -> A[cell[i]] * weights[i], 1:4; init = zero(eltype(A)))
+end
+
+export GridPoint, Cell, interpolate_bilinear, bilinear_weights
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,35 @@ using GridHelpers
 using Test
 
 @testset "GridHelpers.jl" begin
-    # Write your tests here.
-end
+    @testset "Grid points" begin
+        point = GridPoint((5, 5))
+        @test collect(point) == [5, 5]
+        @test point[] == (5, 5)
+    end
+
+    @testset "Cell" begin
+        position = (20.0, 30.0)
+        cell = Cell(position)
+        @test cell.bottom_left == GridPoint(position)
+        @test cell.bottom_right == GridPoint(position .+ (1, 0))
+        @test cell.top_left == GridPoint(position .+ (0, 1))
+        @test cell.top_right == GridPoint(position .+ 1)
+        @test collect(cell) == [cell[1], cell[2], cell[3], cell[4]]
+        weights = bilinear_weights(cell, position)
+        @test weights[1] == 1.0
+        @test sum(weights) == 1
+        @test all(≥(0), weights)
+        weights = bilinear_weights(cell, position .+ 0.5)
+        @test all(==(0.25), weights)
+        weights = bilinear_weights(cell, position .+ (0.13, 0.78))
+        @test sum(weights) == 1
+        @test all(≥(0), weights)
+        @test all(bilinear_weights(cell, cell[i])[i] == 1.0 for i in 1:4)
+        A = zeros(512, 512)
+        @test interpolate_bilinear(A, (4.3, 6.1)) == 0.0
+        A = ones(512, 512)
+        @test interpolate_bilinear(A, (4.3, 6.1)) == 1.0
+        A = rand(512, 512)
+        @test 0 ≤ interpolate_bilinear(A, (4.3, 6.1)) ≤ 1.0
+    end
+end;
